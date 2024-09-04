c7eb9badfa45c26f51b077eb96066110d907b1cd
alumno <alumno@lab_quimica.uacj.mx>
2024-09-04 10:42:25 -0600

    [Agregaron Botonesy Funciones]

--- LolaLaCalculadora/ViewController.swift
+++ LolaLaCalculadora/ViewController.swift
@@ -9,16 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
     //Declaracion del boton y el label a interactuar
-    @IBOutlet weak var btn_Interaccion: UIButton!
+    @IBOutlet weak var btn_Cambia: UIButton!
     @IBOutlet weak var txt_Cambiar: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    //La declaracion de la accion a hacer
-    @IBAction func btn_Pushar_Boton(_ sender: Any) {
-        txt_Cambiar.text="Ya fugaaaaaa"
+    
+    //La declaracion de la accion a hacer y se arrastraron cada boton a esta funcion para que otorgue su numero escrito
+    @IBAction func btn_Numeros(_ sender: UIButton) {
+        txt_Cambiar.text = sender.titleLabel!.text ?? "Nosupe"
+    }
+    
+    //Declaracion del boton operacion
+    @IBAction func btn_Operacion(_ sender: Any) {
+        txt_Cambiar.text = "operacion"
     }
     
 }
