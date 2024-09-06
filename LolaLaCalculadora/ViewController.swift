7d8c3190d28bdf5be9589e37cf2facac685a361f
alumno <alumno@lab_quimica.uacj.mx>
2024-09-06 10:41:14 -0600

    [Al chile no entendi ni madre] Se hizo algo pero ni le entendi muy bien pero medio funge

--- LolaLaCalculadora/ViewController.swift
+++ LolaLaCalculadora/ViewController.swift
@@ -11,20 +11,41 @@ class ViewController: UIViewController {
     //Declaracion del boton y el label a interactuar
     @IBOutlet weak var btn_Cambia: UIButton!
     @IBOutlet weak var txt_Cambiar: UILabel!
+    @IBOutlet weak var btn_ope: UIButton!
+    
+    var botones_Interfaz: Dictionary<String, IUBotonCalculadora>
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        inicializar_Calculadora()
     }
     
     //La declaracion de la accion a hacer y se arrastraron cada boton a esta funcion para que otorgue su numero escrito
     @IBAction func btn_Numeros(_ sender: UIButton) {
-        txt_Cambiar.text = sender.titleLabel!.text ?? "Nosupe"
+        //txt_Cambiar.text = sender.titleLabel!.text ?? "Nosupe"
+        print(botones_Interfaz[(sender.restorationIdentifier ?? btn_ope.restorationIdentifier) ?? "btn"]?.numero ?? "Esto no funciona")
+    }
+    
+    func inicializar_Calculadora() -> Void{
+        crear_arreglos_botones()
+    }
+    
+    func crear_arreglos_botones() -> Void{
+        for numero in 0...9{
+            botones_Interfaz["btn_\(numero)"] = IUBotonCalculadora(
+                "btn_\(numero)",
+                numero: Character("\(numero)"),
+                operacion: ""
+            )
+        }
     }
     
     //Declaracion del boton operacion
     @IBAction func btn_Operacion(_ sender: Any) {
         txt_Cambiar.text = "operacion"
+        
     }
     
 }
