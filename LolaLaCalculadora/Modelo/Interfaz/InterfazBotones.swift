5a8749e4d0f1c74ddb871faeddd9f653966ec725
alumno <alumno@lab_quimica.uacj.mx>
2024-09-11 10:33:21 -0600

    [Modificaciones en la Estructura] Terminar de corregir los ultimos cambios agregados para que si se cambien los numeros al presionar op

--- LolaLaCalculadora/Modelo/Interfaz/InterfazBotones.swift
+++ LolaLaCalculadora/Modelo/Interfaz/InterfazBotones.swift
@@ -6,15 +6,16 @@
 //
 
 import Foundation
+import UIKit
 
 //Interfaz Usuario [Nombre de la clase o estructura]
 struct IUBotonCalculadora {
-    var id: String
+    var referencia_a_boton_interfaz: UIButton?
     var numero: Character
     var operacion: String
     
-    init(_ id: String, numero: Character, operacion: String) {
-        self.id = id
+    init(numero: Character, operacion: String) {
+        self.referencia_a_boton_interfaz = nil
         self.numero = numero
         self.operacion = operacion
     }
@@ -24,52 +25,42 @@ struct IUBotonCalculadora {
         
         
             botones_Interfaz["btn_0"] = IUBotonCalculadora(
-                "btn_0",
                 numero: Character("0"),
                 operacion: ""
             )
             botones_Interfaz["btn_1"] = IUBotonCalculadora(
-                "btn_1",
                 numero: Character("1"),
                 operacion: "+"
             )
             botones_Interfaz["btn_2"] = IUBotonCalculadora(
-                "btn_2",
                 numero: Character("2"),
                 operacion: ""
             )
             botones_Interfaz["btn_3"] = IUBotonCalculadora(
-                "btn_3",
                 numero: Character("3"),
                 operacion: "-"
             )
             botones_Interfaz["btn_4"] = IUBotonCalculadora(
-                "btn_4",
                 numero: Character("4"),
                 operacion: ""
             )
             botones_Interfaz["btn_5"] = IUBotonCalculadora(
-                "btn_5",
                 numero: Character("5"),
                 operacion: ""
             )
             botones_Interfaz["btn_6"] = IUBotonCalculadora(
-                "btn_6",
                 numero: Character("6"),
                 operacion: ""
             )
             botones_Interfaz["btn_7"] = IUBotonCalculadora(
-                "btn_7",
                 numero: Character("7"),
                 operacion: "*"
             )
             botones_Interfaz["btn_8"] = IUBotonCalculadora(
-                "btn_8",
                 numero: Character("8"),
                 operacion: ""
             )
             botones_Interfaz["btn_9"] = IUBotonCalculadora(
-                "btn_9",
                 numero: Character("9"),
                 operacion: "/"
             )
--- LolaLaCalculadora/ViewController.swift
+++ LolaLaCalculadora/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
+enum estados_de_la_calculadora{
+    case seleccionar_numeros
+    case escoger_operacion
+    case mostrar_resultados
+}
+
 class ViewController: UIViewController {
-    var estado_actual:
+    var estado_actual:estados_de_la_calculadora = estados_de_la_calculadora.seleccionar_numeros
     
     //Declaracion del boton y el label a interactuar
     @IBOutlet weak var btn_Cambia: UIButton!
     @IBOutlet weak var txt_Cambiar: UILabel!
     @IBOutlet weak var btn_ope: UIButton!
+    
+    
+    @IBOutlet weak var StackView: UIStackView!
     
     var botones_Interfaz: Dictionary<String, IUBotonCalculadora> = [:]
     var operacion_actual: String? = nil
@@ -27,19 +36,34 @@ class ViewController: UIViewController {
     
     //La declaracion de la accion a hacer y se arrastraron cada boton a esta funcion para que otorgue su numero escrito
     @IBAction func btn_Numeros(_ sender: UIButton) {
-        let txt_añadir = botones_Interfaz[(sender.restorationIdentifier ?? btn_ope.restorationIdentifier) ?? "btn"]?.numero
-        txt_Cambiar.text = "\(txt_Cambiar.text ?? "")\(txt_añadir!)"
+        if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            
+            let txt_añadir = botones_Interfaz[(sender.restorationIdentifier ?? btn_ope.restorationIdentifier) ?? "btn"]?.numero
+            txt_Cambiar.text = "\(txt_Cambiar.text ?? "")\(txt_añadir!)"
+            }
+        else if (estado_actual == estados_de_la_calculadora.escoger_operacion){
+            if let _mensajero: UIButton? = sender{
+                operacion_actual = botones_Interfaz[_mensajero!.restorationIdentifier ?? "btn_0"]?.operacion
+            }
+            else {
+                operacion_actual = nil
+            }
+        }
+        
+        dibujar_numeros_u_operaciones()
     }
-    else if (estado_actual == estado_de_la_calculadora.escoger_operacion){
-        let sender: UIButton
-        operacion_actual == botones_Interfaz[]
+    
+    
+    @IBAction func btn_escoger_operacion(_ sender: UIButton) {
+        if (estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            estado_actual = estados_de_la_calculadora.escoger_operacion
+            dibujar_numeros_u_operaciones()
+        }
     }
-    
-    
-    
     
     func inicializar_Calculadora() -> Void{
         crear_arreglos_botones()
+        identificar_botones()
     }
     
     func crear_arreglos_botones() -> Void{
@@ -47,16 +71,27 @@ class ViewController: UIViewController {
     }
     
     func dibujar_numeros_u_operaciones(){
-        if (estado_actual == estado_de_la_calculadora.escoger_operacion)
+        if (estado_actual == estados_de_la_calculadora.escoger_operacion){
+        }
+        else if (estado_actual == estados_de_la_calculadora.escoger_operacion){
+            for elemento in botones_Interfaz.values{
+                elemento.referencia_a_boton_interfaz?.setTitle(String(elemento.numero), for: .normal)
+            }
+        }
     }
     
-    else if (estado_actual == estade_de_la_calculadora)
-    
-    //Declaracion del boton operacion
-    @IBAction func btn_Operacion(_ sender: Any) {
-        txt_Cambiar.text = "operacion"
+    func identificar_botones(){
+        for pila_de_componentes in StackView.subviews{
+            for botones in pila_de_componentes.subviews{
+                //print(botones.restorationIdentifier)
+                if let identificador = botones.restorationIdentifier{
+                    botones_Interfaz[identificador]?.referencia_a_boton_interfaz = botones as? UIButton
+                }
+            }
+        }
         
+        
+        }
     }
     
-}
 
